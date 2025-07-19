b34693932622b1766c228529f1fbe0fdbff9bd9a
Chase Hard <dchasehardin@gmail.com>
2025-07-19 13:04:43 -0500

    Init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import FromCleanCodeToCopilotPost from "./posts/FromCleanCodeToCopilotPost.mdx";
+import WritingTestsFeelsSlow from "./posts/WritingTestsFeelsSlowPost.mdx";
+
+function App() {
+  return (
+    <div>
+      <FromCleanCodeToCopilotPost />
+      <WritingTestsFeelsSlow />
+    </div>
+  );
+}
+
+export default App;
